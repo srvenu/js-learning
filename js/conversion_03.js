c01fb276983c47d7d854e56402aff1e068842a07
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-10 18:15:14 +0530

    class 03

---
+++ js/conversion_03.js
@@ -0,0 +1,49 @@
+let score = null
+// console.log(typeof score);
+
+let valueInNumber = Number(score); // Type conversion
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// "33" => 33converts 
+// "33abc" => NaN
+// true => 1
+
+let isloggedIn  = 0 //0 => false  1 => true
+let booleanInLoggedIn = Boolean(isloggedIn)
+// console.log(booleanInLoggedIn);
+
+
+// ====================== Operations ========================
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(4/2);
+// console.log(3%2);
+
+let str1 = "hello"
+let  str2 = " Venu"
+// console.log(str1+str2);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+// ++gameCounter
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
