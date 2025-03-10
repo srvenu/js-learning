30d1d74c09210c178c40d6c0349efa0a2d210006
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-10 17:34:32 +0530

    class 3

---
+++ js/datatypes_02.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs not Browser
+// console.log(3 + 3)
+
+// code readability should be high
+
+let name ="Venu S R"
+let age = 21
+let isLoggedIn = false;
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
