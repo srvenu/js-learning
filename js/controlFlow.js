3faf6b5f7ff22cb86c87158c60e6afce3e00f148
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-11 23:24:56 +0530

    class 13

---
+++ js/controlFlow.js
@@ -0,0 +1,36 @@
+// if - else if  - else condition 
+
+// condition < > <= >= == != ===
+/*
+if - else if - else condition flow
+if(condition){
+    code....
+}
+else if(condition){
+    code.....
+}
+else {
+    code......
+}
+
+
+switch
+
+i=1
+switch(i){
+case 1: code... 
+        break;
+case 2: code... 
+        break;
+case 3: code... 
+        break;
+default: code... 
+        break;
+
+}
+*/
+// ============== High Order Array Loop =============
+
+// for of 
+// array iteration
+
