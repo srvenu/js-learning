7622f80b071ec3e8c00fbb557f6413320a279b8e
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-15 21:42:21 +0530

    class 13

--- js/filter_13.js
+++ js/filter_13.js
@@ -12,6 +12,27 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 // } )
 // console.log(newNums)
 
+// const newNumbers = myNums.map((num) => num + 10)
+
+// console.log(newNumbers);
+
+// const newNumbers = myNums
+// .map((num)=> num*10)
+// .map((num)=> num+1)
+
+// console.log(newNumbers);
+
+
+// const myTotal = myNums.reduce((acc,currVal)=>{
+//     console.log(`acc : ${acc}  and  currVal : ${currVal}`);
+//     return acc + currVal 
+// },0)
+
+// console.log(myTotal);
+
+const myTotal = myNums.reduce((acc,curr) => acc+curr,0)
+
+console.log(myTotal);
 
 
 
