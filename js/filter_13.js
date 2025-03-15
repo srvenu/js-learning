51a0da2e9c1e01aeae1f1d744f5995d902262bca
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-15 21:24:30 +0530

    class 13

--- js/filter_13.js
+++ js/filter_13.js
@@ -1,7 +1,17 @@
 const coding =["js","ruby","java","Python","cpp"]
 
 
-coding.forEach( (item)=>{
-    console.log(item);
-} )
+// coding.forEach( (item)=>{
+//     console.log(item);
+// } )
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num)=>{
+//     return num > 4
+// } )
+// console.log(newNums)
+
+
+
 
