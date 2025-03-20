8a9ad227a2f9c33151afbc1959f3c8e73df39dcc
Venu S Raj <srvenugopal2002@gmail.com>
2025-03-20 13:12:04 +0530

    promises class 2

--- js/Promise/promise.js
+++ js/Promise/promise.js
@@ -23,5 +23,78 @@ new Promise(function(resolve,reject){
 })
 
 const promiseThree = new Promise(function(resolve,reject){
-    
+    setTimeout(function(){
+        resolve({username:"srvenu",email:"srvenugopal2002@gmail.com"});
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour =new Promise(function(resolve,reject){
+    setTimeout(function(){
+    let error = true;
+    if(error){
+        resolve({username:'srvenu',password:'1234'});
+    } else{
+        reject('Error: Something went wrong');
+    }
+    },1000)
+})
+
+const userName = promiseFour.then((user)=>{
+    return user.username;
+}).then((username)=>{
+    console.log(username);
+}).catch(function(err){
+    console.log(err);
+}).finally(()=>{
+    console.log("The promise is either resolved or rejected");
+})
+
+const promiseFive =new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:'JavaScript',password:'1234'});
+        } else{
+            reject('Error: JS went wrong');
+        }
+    },1000)
+})
+
+// async awaite
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive();
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ",error);
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);  
+})
+.catch((err)=>{console.log("E: ",err);
 })
