9eb0464ca42a8ef03f9a6b18dea720a894cff635
Venu S Raj <100863025+srvenu@users.noreply.github.com>
2025-03-31 23:03:27 +0530

    Update toPrint.js

--- js/toPrint.js
+++ js/toPrint.js
@@ -8,5 +8,16 @@
 
 <button onclick="window.print()">Print this page</button>
 
+<script>
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+document.getElementById("demo").innerHTML =
+answer1 + "<br>" + 
+answer2 + "<br>" + 
+answer3;
+</script>
+
 </body>
 </html>
